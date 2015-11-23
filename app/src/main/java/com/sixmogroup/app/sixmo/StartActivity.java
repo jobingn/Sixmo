e21f8db28568d248bb896bd97ce3a7ef21142088
jobingn <jobingn@gmail.com>
2015-11-23 10:38:20 +0530

    ver4

--- app/src/main/java/com/sixmogroup/app/sixmo/StartActivity.java
+++ app/src/main/java/com/sixmogroup/app/sixmo/StartActivity.java
@@ -1,9 +1,12 @@
 package com.sixmogroup.app.sixmo;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.sixmogroup.app.sixmo.utils.UserSessionManager;
@@ -15,33 +18,51 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        UserSessionManager sessionManager=new UserSessionManager(getApplicationContext());
-        if(sessionManager.isUserLoggedIn()){
-            Intent eventsIntent;
-            if(sessionManager.getUserRole().equals("user")) {
-                eventsIntent = new Intent(getApplicationContext(), MainActivity.class);
-            }else{
-                eventsIntent = new Intent(getApplicationContext(), AdminMainActivity.class);
-            }
-            startActivity(eventsIntent);
-            this.finish();
-        }
     }
-
+    ProgressBar spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        spinner=(ProgressBar)findViewById(R.id.progressBar);
+        spinner.getIndeterminateDrawable().setColorFilter(Color.WHITE, android.graphics.PorterDuff.Mode.MULTIPLY);
         getSupportActionBar().hide();
-        getStarted=(TextView)findViewById(R.id.buttonGetStarted);
-        getStarted.setOnClickListener(new View.OnClickListener() {
+        new AsyncTask<Void, Void, Void>() {
             @Override
-            public void onClick(View view) {
-                Intent loginIntent=new Intent(getApplicationContext(),LoginActivity.class);
-                startActivity(loginIntent);
-                finish();
+            protected Void doInBackground( final Void ... params ) {
+                // tasks to be completed before starting app
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
             }
-        });
+
+            @Override
+            protected void onPostExecute( final Void result ) {
+                UserSessionManager sessionManager=new UserSessionManager(getApplicationContext());
+                if(sessionManager.isUserLoggedIn()){
+                    Intent eventsIntent;
+                    if(sessionManager.getUserRole().equals("user")) {
+                        eventsIntent = new Intent(getApplicationContext(), MainActivity.class);
+                    }else{
+                        eventsIntent = new Intent(getApplicationContext(), AdminMainActivity.class);
+                    }
+                    startActivity(eventsIntent);
+                    finish();
+                }else {
+                    Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(loginIntent);
+                    finish();
+                }
+            }
+        }.execute();
+
     }
 
 }
